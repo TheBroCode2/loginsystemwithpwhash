a9270e9f57cc7bfb8136a24cbb2eb49c07feb0fe
TheBroCode2 <nikolajdommergaard@gmail.com>
2016-10-01 18:03:18 +0200

    updated
    updated

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php 
 
-$dbCon = new mysqli("nikolajdommergaard.dk.mysql", "nikolajdommerga", "GHAdaJqK", "nikolajdommerga");
+$dbCon = new mysqli("#l", "#", "#", "#");
 
 if (mysqli_connect_error()) {echo "Failed to connect: " . mysqli_connect_error();}
 
